89619dd750aa3e98f5fa2dfb367e98e7013ba4e1
croeer <christian@roeer.info>
2015-07-29 20:24:54 +0200

    use command line parsing lib and implement working version for multiple tables

---
+++ table2csv/Options.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace table2csv
+{
+    class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "Input database to be processed.")]
+        public string InputFile { get; set; }
+
+        [OptionList('t', "tablename", Required = true, Separator = ',', HelpText = "Table(s) to be exported, comma-separated.")]
+        public IList<string> tableName { get; set; }
+
+        [Option('v', "verbose", DefaultValue = false, HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("table2csv", "1.0.0"),
+                Copyright = new CopyrightInfo("croeer\n", DateTime.Now.Year),
+                AdditionalNewLineAfterOption = false,
+                AddDashesToOption = true
+            };
+            help.AddPreOptionsLine("A simple tool to export tables from an MS Access mdb-Database to csv format. " +
+                                   "For each table a separate file will be created.");
+            help.AddOptions(this);
+            return help + "\n";
+        }
+
+    }
+}
--- table2csv/Program.cs
+++ table2csv/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
@@ -13,43 +14,60 @@ namespace table2csv
         static void Main(string[] args)
         {
 
-            var fileName = "AutoTest500.mdb";
-            var tableName = "DAM_ADRESSEN";
+            var options = new Options();
+
+            var result = Parser.Default.ParseArguments(args, options);
+
+            if (!result)
+            {
+                options.GetUsage();
+                return;
+            }
+
+            if (options.Verbose)
+                Console.WriteLine("");
+
+            var fileName = options.InputFile;
 
             string connString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;data source={0}", fileName);
 
-            /*
-            string connString = 
-                "Provider=Microsoft.ACE.OLEDB.12.0;persist security info=True;cache authentication=True;password=zebdepota;user id=zeb;data source=C:\\temp\\AutoTest500.mdb;Jet OLEDB:System database=C:\\dev\\dotnet\\trd-trunk\\bin\\x86_Debug\\trd.mda;Jet OLEDB:Engine Type=5";
-            */
-
             DataTable results = new DataTable();
+            if (options.Verbose)
+                Console.WriteLine("Using connectionstring {0}", connString);
 
             using (OleDbConnection conn = new OleDbConnection(connString))
             {
-                OleDbCommand cmd = new OleDbCommand(String.Format("SELECT * FROM {0}",tableName), conn);
-
                 conn.Open();
 
-                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                foreach (var tableName in options.tableName)
+                {
+                    Console.WriteLine("Exporting table {0}", tableName);
 
-                adapter.Fill(results);
+                    OleDbCommand cmd = new OleDbCommand(String.Format("SELECT * FROM {0}", tableName), conn);
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
 
+                    adapter.Fill(results);
+
+                    StringBuilder sb = new StringBuilder();
+
+                    IEnumerable<string> columnNames = results.Columns.Cast<DataColumn>().
+                                                      Select(column => column.ColumnName);
+                    sb.AppendLine(string.Join(",", columnNames));
+
+                    foreach (DataRow row in results.Rows)
+                    {
+                        IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                        sb.AppendLine(string.Join(",", fields));
+                    }
+                    if (options.Verbose)
+                    {
+                        Console.WriteLine("Saving to {0}.csv", tableName);
+                    }
+
+
+                    File.WriteAllText(string.Format("{0}.csv", tableName), sb.ToString());
+                }
             }
-
-            StringBuilder sb = new StringBuilder();
-
-            IEnumerable<string> columnNames = results.Columns.Cast<DataColumn>().
-                                              Select(column => column.ColumnName);
-            sb.AppendLine(string.Join(",", columnNames));
-
-            foreach (DataRow row in results.Rows)
-            {
-                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
-                sb.AppendLine(string.Join(",", fields));
-            }
-
-            File.WriteAllText(string.Format("{0}.csv", tableName), sb.ToString());
 
         }
 
