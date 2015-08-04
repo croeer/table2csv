9785b33e4b38e6675dc8b55d17999b1b27fb3df0
Christian Roeer <christian@roeer.info>
2015-08-04 15:25:47 +0200

    Add options to dump whole database and only list tables. Refactor to split methods

--- table2csv/Options.cs
+++ table2csv/Options.cs
@@ -13,8 +13,17 @@ namespace table2csv
         [Option('f', "file", Required = true, HelpText = "Input database to be processed.")]
         public string InputFile { get; set; }
 
-        [OptionList('t', "tablename", Required = true, Separator = ',', HelpText = "Table(s) to be exported, comma-separated.")]
+        [OptionList('t', "tablename", Required = false, Separator = ',', HelpText = "Table(s) to be exported, comma-separated.", MutuallyExclusiveSet = "dump")]
         public IList<string> tableName { get; set; }
+
+        [Option('d', "dump", Required = false, HelpText = "Dump the whole database (i.e. all tables)", MutuallyExclusiveSet = "dump")]
+        public bool DumpAll { get; set; }
+
+        [Option('l', "list", Required = false, HelpText = "List all table names.", MutuallyExclusiveSet = "dump")]
+        public bool ListTables { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Ouput directory.")]
+        public string OutputDir { get; set; }
 
         [Option('v', "verbose", DefaultValue = false, HelpText = "Prints all messages to standard output.")]
         public bool Verbose { get; set; }
@@ -24,7 +33,7 @@ namespace table2csv
         {
             var help = new HelpText
             {
-                Heading = new HeadingInfo("table2csv", "1.0.0"),
+                Heading = new HeadingInfo("table2csv", "1.1.0"),
                 Copyright = new CopyrightInfo("croeer\n", DateTime.Now.Year),
                 AdditionalNewLineAfterOption = false,
                 AddDashesToOption = true
--- table2csv/Program.cs
+++ table2csv/Program.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Data.OleDb;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace table2csv
@@ -16,11 +18,28 @@ namespace table2csv
 
             var options = new Options();
 
+            /*
             var result = Parser.Default.ParseArguments(args, options);
-
             if (!result)
             {
-                options.GetUsage();
+                return;
+            }
+            */
+
+            if (!new Parser(new ParserSettings
+            {
+                MutuallyExclusive = true,
+                CaseSensitive = false,
+                HelpWriter = Console.Error
+            }).ParseArguments(args, options))
+            {
+                return;
+            }
+
+            // check mutually exclusive options
+            if (!(options.DumpAll || options.tableName != null || options.ListTables))
+            {
+                Console.WriteLine(options.GetUsage());
                 return;
             }
 
@@ -29,9 +48,20 @@ namespace table2csv
 
             var fileName = options.InputFile;
 
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("File does not exist: {0}.", fileName);
+                return;
+            }
+
+            if (! (options.OutputDir != null) && ! Directory.Exists(options.OutputDir))
+            {
+                Console.WriteLine("Directory does not exist: {0}.", options.OutputDir);
+                return;
+            }
+
             string connString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;data source={0}", fileName);
 
-            DataTable results = new DataTable();
             if (options.Verbose)
                 Console.WriteLine("Using connectionstring {0}", connString);
 
@@ -47,53 +77,121 @@ namespace table2csv
                     return;
                 }
 
-                foreach (var tableName in options.tableName)
+                if (options.ListTables)
                 {
-                    Console.WriteLine("Exporting table {0}", tableName);
-
-                    try
-                    {
-                        OleDbCommand cmd = new OleDbCommand(String.Format("SELECT * FROM {0}", tableName), conn);
-                        OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
-
-                        adapter.Fill(results);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error reading table {0}: {1}", tableName, ex.Message);
-                        continue;
-                    }
-
                     if (options.Verbose)
-                        Console.WriteLine("Parsing {0} rows, {1} columns", results.Rows.Count, results.Columns.Count);
+                        Console.WriteLine("Listing all tables from {0}", fileName);
 
-                    StringBuilder sb = new StringBuilder();
-
-                    IEnumerable<string> columnNames = results.Columns.Cast<DataColumn>().
-                                                      Select(column => column.ColumnName);
-                    sb.AppendLine(string.Join(",", columnNames));
-
-                    foreach (DataRow row in results.Rows)
+                    var tableNameList = getAllTableNames(conn);
+                    if (tableNameList != null)
                     {
-                        IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
-                        sb.AppendLine(string.Join(",", fields));
+                        foreach (var name in tableNameList)
+                        {
+                            Console.WriteLine(name);
+                        }
                     }
-
-                    if (options.Verbose)
-                        Console.WriteLine("Saving to {0}.csv", tableName);
-
-                    try
-                    {
-
-                        File.WriteAllText(string.Format("{0}.csv", tableName), sb.ToString());
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error writing to file {0}.csv\n{1}", tableName, ex.Message);
-                    }
+                    return;
                 }
+                
+                if (options.DumpAll)
+                {
+                    options.tableName = getAllTableNames(conn);
+                }
+
+                if (options.tableName == null)
+                {
+                    Console.WriteLine("Error determining table names. Aborting.");
+                    return;
+                }
+
+                ExportTables(options, conn);
             }
 
+        }
+
+        private static void ExportTables(Options options, OleDbConnection conn)
+        {
+            foreach (var tableName in options.tableName)
+            {
+                Console.WriteLine("Exporting table {0}", tableName);
+
+                var results = GetResults(conn, tableName);
+
+                WriteDatatableToFile(options, results, tableName);
+            }
+        }
+
+        private static void WriteDatatableToFile(Options options, DataTable results, string tableName)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (options.Verbose)
+                Console.WriteLine("Parsing {0} rows, {1} columns", results.Rows.Count, results.Columns.Count);
+
+            IEnumerable<string> columnNames = results.Columns.Cast<DataColumn>().
+                Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in results.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+
+            try
+            {
+                var outputPath = "{0}.csv";
+
+                if (options.OutputDir != null)
+                    outputPath = string.Format("{0}\\{1}", options.OutputDir, outputPath);
+
+                var outputFileName = string.Format(outputPath, tableName);
+
+                if (options.Verbose)
+                    Console.WriteLine("Saving to {0}.", outputFileName);
+
+                File.WriteAllText(outputFileName, sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error writing to file {0}.csv\n{1}", tableName, ex.Message);
+            }
+        }
+
+        private static DataTable GetResults(OleDbConnection conn, string tableName)
+        {
+            DataTable results = new DataTable();
+
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand(String.Format("SELECT * FROM {0}", tableName), conn);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+
+                adapter.Fill(results);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading table {0}: {1}", tableName, ex.Message);
+                return results;
+            }
+            return results;
+        }
+
+        private static IList<string> getAllTableNames(OleDbConnection conn)
+        {
+            try
+            {
+                DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new Object[] { null, null, null, "TABLE" });
+
+                return schemaTable.Rows.OfType<DataRow>().Select(r => r.ItemArray[2].ToString()).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading tables: {0}", ex.Message);
+            }
+
+            return null;
         }
 
 
